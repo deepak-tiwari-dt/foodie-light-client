ab274158976908c36c4e1d5d5a909e34b0669f70
Deepak Tiwari <deepak.tiwari.d21@gmail.com>
2024-09-10 18:26:10 +0530

    Modified Pagination

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -157,17 +157,20 @@ const RestaurantList = () => {
           ))
         )}
       </div>
-      <div className="pagination">
-        {Array.from({ length: totalPages }, (_, index) => (
-          <button
-            key={index + 1}
-            onClick={() => handlePageChange(index + 1)}
-            className={currentPage === index + 1 ? "active" : ""}
-          >
-            {index + 1}
-          </button>
-        ))}
-      </div>
+
+      {!editingRestaurant && (
+        <div className="pagination">
+          {Array.from({ length: totalPages }, (_, index) => (
+            <button
+              key={index + 1}
+              onClick={() => handlePageChange(index + 1)}
+              className={currentPage === index + 1 ? "active" : ""}
+            >
+              {index + 1}
+            </button>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
